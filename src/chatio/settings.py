79000b8f7a99b7c702fa4ece8b3e7f8b341193df
LaurenceZanotti <lau_zanotti@hotmail.com>
2022-01-31 14:24:43 -0300

    Tutorial Part 4: Automated Testing (with some fixes)

--- src/chatio/settings.py
+++ src/chatio/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from os.path import join
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,7 +79,10 @@ WSGI_APPLICATION = 'chatio.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': join(BASE_DIR, 'db.sqlite3'),
+        'TEST': {
+            'NAME': join(BASE_DIR, 'db_test.sqlite3')
+        }
     }
 }
 
